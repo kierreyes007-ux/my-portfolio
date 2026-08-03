9c40e571c01ada6ecfdb01f2d34fc2c51e383b58
Tech Wiz <techwhiz0101@gmail.com>
2026-08-03 16:25:44 +0800

    Update hero section styling

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -9,36 +9,39 @@ function Hero() {
     data-aos-duration="1000"
     data-aos-easing="ease-out-cubic">
       <div >
-        <h1 className="text-4xl md:text-6xl font-bold">
+        <h1 className="text-4xl md:text-6xl font-bold px-5 pt-7 md:pt-1">
           Hi, I'm <span className="text-blue-600">Kier</span>
         </h1>
 
-        <p className="mt-4 max-w-xl text-lg md:text-2xl text-gray-600">
+        <p className="mt-4 max-w-xl px-5 text-lg md:text-2xl text-gray-600">
          Frontend Developer focused on building responsive and user-friendly web applications using React and Tailwind CSS. I enjoy turning ideas into clean and modern interfaces.
         </p>
 
         <div className="mt-6 flex flex-col items-center sm:flex-row gap-4 justify-center md:justify-start">
 
-          <a href="#projects">
-          <button className="bg-black text-white px-6 py-3 rounded-md hover:bg-gray-800 transition"
-          >
-            View Projects
-          </button>
-          </a>
+  <a 
+    href="#projects"
+    className="bg-black text-white px-6 py-3 rounded-md hover:bg-gray-800 transition"
+  >
+    View Projects
+  </a>
 
-            <a href="#contact">
-          <button className="border border-black px-6 py-3 rounded-md hover:bg-black hover:text-white transition">
-            Contact Me
-          </button>
-            </a>
-            
-            <a href="/resume.pdf"
-            download="resume.pdf"
-            className="bg-blue-600 text-white px-6 py-3 rounded-md hover:bg-blue-700 transition"
-          >
-            Download Resume
-          </a>
-        </div>  
+  <a
+    href="/resume.pdf"
+    download="resume.pdf"
+    className="border border-blue-600 text-blue-600 px-6 py-3 rounded-md hover:bg-blue-600 hover:text-white transition"
+  >
+    Download Resume
+  </a>
+
+  <a
+    href="#contact"
+    className="border border-gray-400 px-6 py-3 rounded-md hover:bg-white transition"
+  >
+    Contact Me
+  </a>
+
+</div>
       </div>
 
       <div>
