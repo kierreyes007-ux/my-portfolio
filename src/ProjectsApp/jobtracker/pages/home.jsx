da5c46f622c71d8e310e8ea4c11371c10b85a6fa
Tech Wiz <techwhiz0101@gmail.com>
2026-09-25 07:04:44 +0800

    another url navigation correction inside jobs

--- src/ProjectsApp/jobtracker/pages/home.jsx
+++ src/ProjectsApp/jobtracker/pages/home.jsx
@@ -85,7 +85,7 @@ function Home() {
                     <div className="w-full flex justify-between px-10 bg-white my-3 shadow-lg rounded-sm py-15 "><p className="text-3xl font-semibold">No Applications</p>
                     <button
                             
-                            onClick={() => navigate("/jobs")}
+                            onClick={() => navigate("/projects/job-tracker/jobs")}
                             className=" items-center gap-2 px-4 py-2 bg-gray-50 text-black text-sm font-medium rounded-md hover:bg-gray-200 transition-colors"
                         >
                             <i className="fa-solid fa-plus text-xs"></i>
